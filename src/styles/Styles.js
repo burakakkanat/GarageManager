13afb75b66adab329a131310b72c04200ad2f4d5
Burak Akkanat <burakakkanat@windowslive.com>
2023-03-02 00:47:33 +0300

    Dropdown container height adjustment.

--- src/styles/Styles.js
+++ src/styles/Styles.js
@@ -63,6 +63,7 @@ const styles = StyleSheet.create({
         padding: 8,
     },
     containerPickerAddVehicle: {
+        height: '93%',
         width: '54%'
     },
     containerPickerWishlist: {
